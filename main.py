1f173cf9bc618b76341dc74c4ca7cf2e35929276
keimos <keithmosleyjr@gmail.com>
2025-02-11 22:17:53 -0500

    Added code for recommending items

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ sparse_matrix = csr_matrix(user_item_matrix)
 # Compute cosine similarity between users
 user_similarity = cosine_similarity(sparse_matrix)
 user_similarity_df = pd.DataFrame(user_similarity, index=user_item_matrix.index, columns=user_item_matrix.index)
+
+def recommend_items(user_id, user_item_matrix, user_similarity_df, top_n=5):
+    # Get the similarity scores for the given user
+    similar_users = user_similarity_df[user_id].sort_values(ascending=False)[1:]  # Exclude the user itself
+
+    # Weighted sum of ratings from similar users
+    recommendations = user_item_matrix.mul(similar_users, axis=0).sum(axis=0) / similar_users.sum()
+
+    # Recommend items that the user hasn't rated yet
+    items_to_recommend = recommendations[user_item_matrix.loc[user_id] == 0].sort_values(ascending=False)
+
+    return items_to_recommend.head(top_n)
+
+# Example: Recommend items for user_id 1
+print(recommend_items(user_id=1, user_item_matrix=user_item_matrix, user_similarity_df=user_similarity_df))
